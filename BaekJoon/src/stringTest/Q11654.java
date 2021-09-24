7ed982c93dab877b0a585cb23b80c81e52232894
Rp <rp177@naver.com>
2021-09-24 10:11:21 +0900

    0924 시작

--- BaekJoon/src/stringTest/Q11654.java
+++ BaekJoon/src/stringTest/Q11654.java
@@ -1,10 +1,21 @@
 package stringTest;
 
+import java.util.Scanner;
+
 public class Q11654 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.nextLine();
+		char[] cc = s.toCharArray();
+		char c = cc[0];
+		
+		System.out.println(c);	
+				
+		sc.close();
+		
 	}
 
 }
