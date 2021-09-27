473de4b934f199b668a04149703761ab85594058
RP <rp177@naver.com>
2021-09-27 23:18:11 +0900

    0927 마무리 1316 풀어야해

--- BaekJoon/src/stringTest/Q1316.java
+++ BaekJoon/src/stringTest/Q1316.java
@@ -1,9 +1,21 @@
 package stringTest;
 
+import java.util.Scanner;
+
 public class Q1316 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		int N = Integer.parseInt(sc.nextLine());
+		
+		for ( int i = 0 ; i < N ; i ++ ) {
+			
+		}
+		
+		
+		sc.close();
 	}
 
 }
