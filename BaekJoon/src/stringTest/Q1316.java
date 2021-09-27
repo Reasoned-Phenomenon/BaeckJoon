3867f826fef99a43d779ff853aa16dabb2a31a3b
Rp <rp177@naver.com>
2021-09-28 08:50:46 +0900

    0928 수업 시작 전

--- BaekJoon/src/stringTest/Q1316.java
+++ BaekJoon/src/stringTest/Q1316.java
@@ -1,6 +1,8 @@
 package stringTest;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Q1316 {
 
@@ -9,11 +11,39 @@ public class Q1316 {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = Integer.parseInt(sc.nextLine());
+		String s = null;
+		String[] ss = null;
+		int cnt = 0 ;
+		int match = 0;
+		Set<String> set = new HashSet<>();
 		
 		for ( int i = 0 ; i < N ; i ++ ) {
 			
+			s = sc.nextLine();
+			ss = s.split("");
+			
+			for (int n = 0 ; n < ss.length ; n++) {
+				set.add(ss[n]);
+			}
+			
+			for (int n = 1 ; n <= ss.length ; n++) {
+				
+				if ( ! ss[n-1].equals(ss[n])) {
+					cnt++;
+				}
+				
+			}
+			
+			int size = set.size();
+			System.out.println("===num " + size);
+			System.out.println("===cnt " + cnt);
+			if ( cnt == size) {
+				match++;
+			}
+			
 		}
 		
+		System.out.println(match);
 		
 		sc.close();
 	}
