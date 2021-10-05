1b159194312694122a76fee6cc1617bbba9b80d6
RP <rp177@naver.com>
2021-10-05 21:02:49 +0900

    1005 Q10757 조금만 더 생각해보기

--- BaekJoon/src/basicMath1/Q10757.java
+++ BaekJoon/src/basicMath1/Q10757.java
@@ -10,20 +10,45 @@ public class Q10757 {
 		
 		String s = sc.nextLine();
 		String[] ss = s.split(" ");
-		try {
-			long A = Long.parseLong(ss[0]);
-			System.out.println(A);
-			long B = (long) Long.parseLong(ss[1]);
-			System.out.println(B);
-			
-			long AB = A+B;
-			
-			System.out.println(AB);
-			
-		} catch(Exception e) {
-			e.getStackTrace();
-		}
 		
+		String a = ss[0];
+		String b = ss[1];
+		
+		String[] aa = null;
+		String[] bb = null;
+		int[] ab = null;
+		
+		if (a.length() > 18 || b.length() > 18) {
+			
+			for ( int i = 0 ; i < a.length();i++) {
+				aa[i] = a.substring(a.length()-i, a.length()-i);
+			}
+			
+			for ( int i = 0 ; i < b.length();i++) {
+				bb[i] = a.substring(b.length()-i, b.length()-i);
+			}
+			
+			for (int i = 0; i<a.length() || i<b.length() ; i ++) {
+				if (aa[i] == null) {
+					aa[i]="0";
+				}
+				if (bb[i] == null) {
+					bb[i]="0";
+				}
+				ab[i] = Integer.parseInt(aa[i])+Integer.parseInt(bb[i]);
+			}
+			for (int i = 0 ; i< ab.length ; i ++) {
+				if( ab[i] >= 10) {
+					ab[i]--;
+					ab[i+1]++;
+				}
+			}
+			
+			
+			
+		} else {
+			System.out.println(Integer.parseInt(a)+Integer.parseInt(b));
+		}
 		
 		sc.close();
 	}
